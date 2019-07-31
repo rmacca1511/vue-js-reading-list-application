4eb35c494f2930edf1fe401669c6d1327be3acac
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-31 12:36:45 +0100

    finished work

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,37 +2,47 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books">{{ book.title }}: {{ book.author }}</li>
+        <book-item v-for="(book, index) in books" :book='book' :key="index"></book-item>
     </ul>
+    <hr>
+    <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'BookList',
-    data() {
-      return {
-        title: 'All Books',
-        books: [
-            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-            {title: 'American Gods', author: 'Neil Gaiman'},
-            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-        ]
-      }
+import BookItem from './BookItem';
+import BookForm from './BookForm';
+
+export default {
+  name: 'BookList',
+  components: {
+    BookItem,
+    BookForm
+  },
+  data() {
+    return {
+      title: 'All Books',
+      books: [
+          {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+          {title: 'American Gods', author: 'Neil Gaiman'},
+          {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+      ]
+    }
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor })
     }
   }
+}
 </script>
 
-<style lang="scss" scoped>
+<style>
 h1, h2 {
   font-weight: normal;
 }
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: block;
-  margin: 0 10px;
 }
 </style>
